00772d67109a80fb7f812fe877c56544fbd8b92d
Radames Ajna <radamajna@gmail.com>
2018-11-29 14:18:27 -0800

    change project name

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     sourceMap: true
   },
 
-  baseUrl: '/teens-vs-spotify/',
+  baseUrl: '/can-u-teach-ai-to-dance/',
   outputDir: undefined,
   assetsDir: 'src/assets',
   runtimeCompiler: undefined,
